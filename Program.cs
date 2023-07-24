ce65bec3e6c4b690a4377a375fcfa7e657a2176b
NijatAziz <nijatfaz@code.edu.az>
2023-07-24 14:55:23 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+public class Program
+{
+    public static string CapitalizeSentence(string sentence)
+    {
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        return textInfo.ToTitleCase(sentence.ToLower());
+    }
+
+    public static void Main()
+    {
+        string originalSentence = "bU tasKI metHodla yAzmAliSiz";
+        string capitalizedSentence = CapitalizeSentence(originalSentence);
+        Console.WriteLine(capitalizedSentence);
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static int Add3ToLeft(int number)
+    {
+        int powerOf10 = 1;
+        while (powerOf10 <= number)
+            powerOf10 *= 10;
+
+        return 3 * powerOf10 + number;
+    }
+
+    public static void Main()
+    {
+        int originalNumber = 8675;
+        int modifiedNumber = Add3ToLeft(originalNumber);
+        Console.WriteLine(modifiedNumber); 
+    }
+}
